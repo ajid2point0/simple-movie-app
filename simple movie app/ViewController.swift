450be54fb5e1eaa7f6930284272ecf003196d41b
Abderrahman ajid <abderrahman.ajid@gmail.com>
2022-03-09 12:06:38 +0100

    Initial Commit

---
+++ simple movie app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple movie app
+//
+//  Created by Abderrahman Ajid on 9/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
